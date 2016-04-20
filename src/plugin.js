07c4072b22b7ebb31da28ab163354a783cd90866
Nate Kresge <kresge@neon-lab.com>
2016-04-19 18:13:09 -0700

    Implement tracker plugin for playlist videojs (handle multiple vids played on one load

--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,10 @@
-/* eslint indent:0 */
+/* eslint indent:0 no-console:0*/
 'use strict';
 
 import videojs from 'video.js';
 import reqwest from 'reqwest';
 import printf from 'printf';
+import _ from 'lodash';
 
 // Package scope references to plugin and player
 let neon;
@@ -49,13 +50,6 @@ const constants = {
     }
 };
 
-// Dummy dev params TODO
-const dummyData = {
-    // From the other trackers grab this from their api?
-    // Video identifier
-    vid: 'alskdjf987'
-};
-
 // Get the urlencoded location of the video player page
 const _getPageUrl = () => {
     return encodeURIComponent(window.location.href);
@@ -85,41 +79,63 @@ const _isInAdState = () => {
     return _adStateRegex.test(player.el().className);
 };
 
-const _sendToConsole = (message) => {
-    if (neon.options.showConsoleLogging) {
-        console.info(message);
-    }
-};
+// Specify valid keys for the api to guard against invalid request
+const _trackerAllowedParams = [
+    'a',
+    'acount',
+    'adelta',
+    'aplay',
+    'bn',
+    'bns',
+    'cts',
+    'page',
+    'pageid',
+    'pcount',
+    'playerId',
+    'prcnt',
+    'ref',
+    'tai',
+    'ttype',
+    'vid'
+];
 
 // Run a ajax request for the log data
-const _remoteLogEvent = (eventType, extra) => {
-    let action = constants.eventCodeMap[eventType];
-    let data = videojs.mergeOptions(
+const _sendToTracker = (eventType, eventDetails) => {
+    const action = constants.eventCodeMap[eventType];
+    let pcount;
 
-        // @TODO
-        dummyData,
+    if(neon.currentVid === undefined) {
+        pcount = null;
+    } else {
+        pcount=  neon.playedVids.indexOf(neon.currentVid) + 1;
+    }
+    const data = _.pick(videojs.mergeOptions(
 
+        // Page-level data
         neon.pageData,
-        // Event-level data
+
+        // Common event-level data
         {
             // Action identifier
             a: action,
             // Client's timestamp in localtime, in millis
             cts: (new Date()).getTime(),
             // 1-based index of video for videos in page
-            // @TODO
-            pcount: 1,
-            ref: _getReferrer()
+            pcount: pcount,
+            ref: _getReferrer(),
+            vid: neon.currentVid
         },
-        // Event-type-specific extra data
-        extra
-    );
 
-    let url = neon.options.trackUrl;
+        // Event-type-specific data from eventDetails
+        eventDetails
 
-    _sendToConsole(printf('%s -> %s', eventType, action), url, data);
+        // And filter unrecognized params
+    ), _trackerAllowedParams);
 
-    // Implement the rest of tracked data
+    if (neon.options.showConsoleLogging) {
+        console.log(printf('%s -> %s', eventType, action), data);
+    }
+
     reqwest({
         url: neon.options.trackUrl,
         method: 'GET',
@@ -129,44 +145,31 @@ const _remoteLogEvent = (eventType, extra) => {
 };
 
 // Check if event needs remote tracking in configured trackEvents
-const _commonTrack = (playerEvent, extra) => {
-    extra = extra || {};
-    if (neon.options.trackEvents.indexOf(playerEvent.type) >= 0) {
-        _remoteLogEvent(playerEvent.type, extra);
+const _commonTrack = (neonEventType, eventDetails) => {
+    eventDetails = eventDetails || {};
+    if (neon.options.trackEvents.indexOf(neonEventType) >= 0) {
+        _sendToTracker(neonEventType, eventDetails);
     }
 };
 
-const trackImageLoad = (playerEvent, extra) => {
-    let url;
-    let width;
-    let height;
-
-    extra = extra || {};
-    if (extra.image !== undefined) {
-        url = extra.image.url;
-        width = extra.image.width;
-        height = extra.image.height;
-    } else if (player.poster() !== undefined) {
-        url = player.poster();
-        width = player.posterImage.width();
-        height = player.posterImage.height();
-    } else {
-        // Can't effectively track without more info
-        return;
-    }
-    extra.bns = printf('%s %d %d',
-        _getBasenameOf(url), width, height);
-    _commonTrack(playerEvent, extra);
-};
-
-const trackPlay = (playerEvent, extra) => {
+const trackPlay = (playerEvent, eventDetails) => {
     if (_isInAdState()) {
         return;
     }
+
+    neon.currentVid = _extractVideoId();
+    if(neon.playedVids.indexOf(neon.currentVid) < 0) {
+        neon.playedVids.push(neon.currentVid);
+        neon.percentsPlayed[neon.currentVid] = {};
+    }
+
     neon.hasVidPlayed = true;
-    extra = extra || {aplay: false};
-    extra.adplay = neon.hasAdPlayed;
-    _commonTrack(playerEvent, extra);
+    eventDetails = eventDetails || {};
+    // Set no-autoplay flag
+    eventDetails.aplay = false;
+    // Set ad view flag
+    eventDetails.adplay = neon.hasAdPlayed;
+    _commonTrack('play', eventDetails);
 };
 
 const guessAutoplay = (e) => {
@@ -178,39 +181,100 @@ const guessAutoplay = (e) => {
     }
 };
 
-const trackImageView = (playerEvent) => {
-    _commonTrack(playerEvent);
+// Build basenames param string given list of map {url, width, height}
+const _buildBnsParamFromList = (list) => {
+    const values = [];
+
+    list.forEach((dict) => {
+        // Use space delimiter; the param will be urlencoded later
+        values.push(printf('%s %d %d',
+            _getBasenameOf(dict.url),
+            dict.width,
+            dict.height)
+        );
+    });
+
+    return values.join(',');
 };
 
-const trackImageClick = (playerEvent) => {
-    _commonTrack(playerEvent);
+const _getImagesFromEvent = (details) => {
+    let url;
+    let width;
+    let height;
+    const values = [];
+
+    if (details.images !== undefined) {
+        details.images.forEach((image) => {
+            values.push({url, width, height});
+        });
+    } else if (player.poster() !== undefined) {
+        url = player.poster();
+        width = player.posterImage.width();
+        height = player.posterImage.height();
+        values.push({url, width, height});
+    }
+    return values;
 };
 
-const trackAdPlay = (playerEvent) => {
+const trackImageLoad = (playerEvent, eventDetails) => {
+    eventDetails = eventDetails || {};
+    const images = _getImagesFromEvent(eventDetails);
+
+    if (images.length === 0) {
+        console.log('Abort log player image load event: not enough info to continue');
+        return;
+    }
+    eventDetails.bns = _buildBnsParamFromList(images);
+    _commonTrack('imageLoad', eventDetails);
+};
+
+const trackImageView = (playerEvent, eventDetails) => {
+    eventDetails = eventDetails || {};
+    const images = _getImagesFromEvent(eventDetails);
+
+    if (images.length === 0) {
+        console.log('Abort log player image load event: not enough info to continue');
+        return;
+    }
+    eventDetails.bns = _buildBnsParamFromList(images);
+    _commonTrack('imageView', eventDetails);
+};
+
+const trackImageClick = (playerEvent, eventDetails) => {
+    eventDetails = eventDetails || {};
+    const images = _getImagesFromEvent(eventDetails);
+
+    if (images.length === 0) {
+        console.log('Abort log player image load event: not enough info to continue');
+        return;
+    }
+    eventDetails.bn = _buildBnsParamFromList(images);
+    _commonTrack('imageClick', eventDetails);
+};
+
+const trackAdPlay = (playerEvent, eventDetails) => {
     if (neon.hasAdPlayed) {
         return;
     }
     neon.hasAdPlayed = true;
-    _commonTrack(
-        {type: 'adPlay'},
-        {aplay: false}
-    );
+    _commonTrack('adPlay', {aplay: false});
 };
 
 const trackVideoViewPercent = (playerEvent) => {
 
     // Measure the play progress by interval set in options
     const interval = Math.min(100, neon.options.timeUpdateInterval);
+    const vid = neon.currentVid;
     // Begin at the first interval and not zero.
     let percent = interval;
 
     // Check for each step in [0..100], and keep
     // track of those we've already sent.
     for (; percent <= 100; percent += interval) {
-        if (_getPercentPlayed() > percent) {
-            if (neon.percentsPlayed.indexOf(percent) < 0) {
-                neon.percentsPlayed.push(percent);
-                _commonTrack(playerEvent, {prcnt: percent});
+        if (_getPercentPlayed() >= percent) {
+            if (neon.percentsPlayed[vid].indexOf(percent) < 0) {
+                neon.percentsPlayed[vid].push(percent);
+                _commonTrack('timeUpdate', {prcnt: percent});
             }
         }
     }
@@ -230,6 +294,35 @@ const _uuid = () => {
     const _alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     return randomString(16, _alphanum);
+};
+
+const _extractVideoId = () => {
+    // Extract the video id
+    const idKey = neon.options.publisherIdAttribute;
+    let videoId = player.el().getAttribute(idKey);
+
+    if (videoId === null) {
+        throw new Error(
+            'Fatal config error: player has no publisher id for key ' + idKey
+        );
+    }
+    const regex = neon.options.publisherIdAttributeRegex;
+
+    // Check the usefulness of this regex code
+    if (regex !== undefined && regex !== null) {
+        const matches = videoId.match(regex);
+
+        if (matches !== null) {
+            videoId = matches[0];
+        } else {
+            throw new Error(printf(
+                'Fatal config error: video id %s does not match pattern %s',
+                videoId,
+                regex
+            ));
+        }
+    }
+    return videoId;
 };
 
 /**
@@ -252,28 +345,34 @@ const onPlayerReady = (player_, options) => {
         // Tracking type
         ttype: defaults.trackingType
     };
-    neon.percentsPlayed = [];
+
+    // Store state of videos played
+    neon.playedVids = [] 
+    neon.percentsPlayed = {};
     neon.hasAdPlayed = false;
     neon.hasVidPlayed = false;
 
-    player.on('imageLoad', trackImageLoad);
-    player.on('imageView', trackImageView);
-    player.on('imageClick', trackImageClick);
+    player.on('image_load', trackImageLoad);
+    player.on('image_view', trackImageView);
+    player.on('image_click', trackImageClick);
 
     let posterUrl = player.poster();
 
     if (posterUrl !== undefined) {
         trackImageLoad({type: 'imageLoad'});
+        trackImageView({type: 'imageView'});
+        // Treat a play event as a image click for the poster
+        player.one('play', trackImageClick);
     }
 
-    // Capture play events just for the first play.
-    player.one('play', trackPlay);
-    player.one('ad-play', trackAdPlay);
-    player.one('ima3-started', trackAdPlay);
-    player.one('timeUpdate', guessAutoplay);
+    player.on('play', trackPlay);
+    player.on('ad-play', trackAdPlay);
+    player.on('ima3-started', trackAdPlay);
 
-    player.on('timeUpdate', trackVideoViewPercent);
+    // Use timeupdate for detecting autoplay
+    player.one('timeupdate', guessAutoplay);
 
+    player.on('timeupdate', trackVideoViewPercent);
     player.on(['adstart', 'ads-ad-started', 'ima3-started'], trackAdPlay);
 };
 
